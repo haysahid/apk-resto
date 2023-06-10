876672209b6e71c8d73aad58f1038e3145638b79
Sahid Anwar <hay.sahid@gmail.com>
2023-06-10 14:01:45 +0700

    feat: add DB connection

--- Controls/ClsCtlKategori.vb
+++ Controls/ClsCtlKategori.vb
@@ -1,3 +1,23 @@
-﻿Public Class ClsCtlKategori
+﻿Imports System.Data.Odbc
+Public Class ClsCtlKategori : Implements InfProses
 
+    Public Function InsertData(Ob As Object) As OdbcCommand Implements InfProses.InsertData
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function UpdateData(Ob As Object) As OdbcCommand Implements InfProses.UpdateData
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function DeleteData(kunci As String) As OdbcCommand Implements InfProses.DeleteData
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function tampilData() As DataView Implements InfProses.tampilData
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function cariData(kunci As String) As DataView Implements InfProses.cariData
+        Throw New NotImplementedException()
+    End Function
 End Class
--- Controls/InfProses.vb
+++ Controls/InfProses.vb
@@ -1,3 +1,8 @@
-﻿Public Interface InfProses
-
+﻿Imports System.Data.Odbc
+Public Interface InfProses
+    Function InsertData(Ob As Object) As OdbcCommand
+    Function UpdateData(Ob As Object) As OdbcCommand
+    Function DeleteData(kunci As String) As OdbcCommand
+    Function tampilData() As DataView
+    Function cariData(kunci As String) As DataView
 End Interface
